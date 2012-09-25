f21f334e16a9e5b3115b5a7ad453ee07c1b06964
henrique <henrique@henrique>
2012-09-25 02:16:17 -0300

    validacoes iniciais

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
+  
+  validates_presence_of :cpf, :senha, :rg, :nome, :email_1, :dica_senha, :resposta_dica_senha
+  validates_uniqueness_of :cpf, :rg, :email_1, :email_2
+   
+  validates_length_of :cpf, :is => 11
+  
+  validates_length_of :senha, :minimum => 6
+  
+  validates_length_of :rg, :in => 8..9
+
+  
   attr_accessible :cpf, :dica_senha, :email_1, :email_2, :nome, :resposta_dica_senha, :rg, :senha, :sistema, :status, :usertype
 end
