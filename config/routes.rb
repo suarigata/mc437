1d2acdfd2d032a2151baa9b7e8eefb63ef0e1208
alex <alex@mailinator.com>
2012-09-26 16:08:25 -0300

    Página inicial vazia para logados e alguns itens no menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Auth::Application.routes.draw do
+  get "home/index"
+
   resources :logins
 
   resources :log_gerals
@@ -12,6 +14,8 @@ Auth::Application.routes.draw do
   resources :usertypes
 
   resources :sistemas
+  
+  # logout '/logout', :controller => '', :action => 'destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -62,7 +66,7 @@ Auth::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'login#new'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
